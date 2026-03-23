992f1a91a65c02eef6a2418e47464b99778f41da
heimian0722-hash <heimian0722@gmail.com>
2026-03-23 14:42:32 +0800

    Update ikuai-bypass-openwrt.sh

--- ikuai-bypass-openwrt.sh
+++ ikuai-bypass-openwrt.sh
@@ -9,23 +9,25 @@
 # 配置github代理 如果不可用请自行更换如果已经有直连github环境也可以去掉这行
 export GhProxy=https://ghp.ci/
 
-# 切换到opt目录
+#创建切换到opt目录
 mkdir -p /opt/ && cd  /opt/
+
 #下载ikuai-bypass文件，v4.2是支持爱快3.7系统上限，爱快4.0及以上请使用V4.3以上版本.
 wget -O ikuai-bypass-linux-amd64.zip "${GhProxy}https://github.com/joyanhui/ikuai-bypass/releases/download/v4.2.0/ikuai-bypass-linux-amd64.zip"
-
 # 解压文件，删除无关文件
 unzip ikuai-bypass-linux-amd64.zip && rm -rf ikuai-bypass-linux-amd64.zip && rm -rf README.md
 
-# 使用版本内的配置文件config.yml或者自己写好配置文件直接上传/opt, 上传记得加权限chmod +x /opt/ikuai-bypass
+# 自己上传config.yml到/opt, 给ikuai-bypass
+加权限chmod +x /opt/ikuai-bypass
 
-# 或者用最新的演示配置
+# 在线获取最新的演示配置，可能不兼容
 wget ${GhProxy}https://raw.githubusercontent.com/joyanhui/ikuai-bypass/main/config.yml -O config.yml
+
+# 手动执行一次 检查执行结果(需要注意自己配置的模式，下面这条是混合模式，默认运营商模式)
+/opt/ikuai-bypass -c /opt/config.yml -r 1 -m ii
 
 # 更新或者下载最新版到 /opt/注意修改版本号CPU架构以及路径  =================================== end
 
-# 手动执行一次 检查执行结果(需要注意自己配置的模式，下面这条是混合模式，默认运营商模式)
-/opt/ikuai-bypass -c /opt/config.yml -r 1 -m ii 
 
 # 创建服务脚本，这段代码请整体复制后粘贴，或者使用vim nano编辑  ================================= start
 cat > /etc/init.d/ikuai-bypass << \EOF
